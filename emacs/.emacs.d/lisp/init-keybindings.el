56d7eab0c14f75f22c712b6ceb9353ce1e0bb035
yanhao.cyh <yanhao.cyh@antfin.com>
2020-09-29 17:35:03 +0800

    config keybindings

--- emacs/.emacs.d/lisp/init-keybindings.el
+++ emacs/.emacs.d/lisp/init-keybindings.el
@@ -1,43 +1,43 @@
 
-(global-set-key (kbd "C-s") 'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
+;; EMACS 快捷键绑定规范
+;; In summary, the general rules are:
+;; - C-x reserved for Emacs native essential keybindings: buffer, window, frame, file, directory, etc...
+;; - C-c reserved for user and major mode:
+;;   * C-c letter reserved for user. <F5>-<F9> reserved for user.
+;;   * C-c C-letter reserved for major mode.
+;; - Don't rebind C-g, C-h and ESC.
+
+
+;; 搜索相关
+(global-set-key (kbd "C-f") 'swiper) ;; 单文件中grep
+(global-set-key (kbd "C-S-f") 'counsel-git-grep) ;; 在一个git项目中grep
+(global-unset-key "\C-c\ p") ;; evil-nerd-commenter 插件默认将 C-c p 绑定了，这里要先解绑，不然后面绑定 C-c p f 报错
+(global-set-key (kbd "C-c p f") 'counsel-git) ;; 在一个git项目中find-file
+
+
 (global-set-key (kbd "M-x") 'counsel-M-x)
+
+(global-set-key "\C-c\ \C-r" 'recentf-open-files)
+
+(global-set-key (kbd "M-s o") 'my-occur-dwim)
+
+
+(global-set-key (kbd "C-c =") 'indent-region-or-buffer)
+
+(global-set-key (kbd "C-=") 'er/expand-region)
+(global-set-key (kbd "C--") 'er/contract-region)
+
+(global-set-key (kbd "C-c a") 'org-agenda)
+
+
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (global-set-key (kbd "C-h f") 'counsel-describe-function) ;; 跳到函数定义
 (global-set-key (kbd "C-h v") 'counsel-describe-variable)
-(global-set-key (kbd "C-c f") 'counsel-git)
-(global-set-key (kbd "C-c t") 'counsel-git-grep)
-;; (global-set-key (kbd "C-c p f") 'counsel-git)
-(global-set-key (kbd "M-s m") 'counsel-imenu)
-
-(global-set-key (kbd "C-;") 'iedit-mode)
-(global-set-key (kbd "M-s o") 'my-occur-dwim)
-
-(global-set-key (kbd "C-x k") 'kill-buffer)
-
-
-(global-set-key "\C-x\ \C-r" 'recentf-open-files)
-
-(global-set-key (kbd "C-c s o") 'sr-speedbar-open)
-(global-set-key (kbd "C-c s c") 'sr-speedbar-close)
-
 ;; 注意和 C-h f/v/k 的区别，它们是describe function/variable/key，
 ;; 而这里是 find function/variable/key
 (global-set-key (kbd "C-h C-f") 'find-function)
 (global-set-key (kbd "C-h C-v") 'find-variable)
 (global-set-key (kbd "C-h C-k") 'find-function-on-key)
-
-
-;; ggtags
-(global-set-key (kbd "C-c g s") 'ggtags-find-other-symbol)
-(global-set-key (kbd "C-c g h") 'ggtags-view-tag-history)
-(global-set-key (kbd "C-c g r") 'ggtags-find-reference)
-(global-set-key (kbd "C-c g f") 'ggtags-find-file)
-(global-set-key (kbd "C-c g c") 'ggtags-create-tags)
-(global-set-key (kbd "C-c g u") 'ggtags-update-tags)
-(global-set-key (kbd "M-,") 'pop-tag-mark)
-
-
 
 
 
@@ -50,17 +50,13 @@
 				    (interactive)
 				    (switch-to-buffer (other-buffer (current-buffer) 1))))
 
-
 ;; 切换 buffer
 (global-set-key (kbd "M-<left>") 'previous-buffer)
 (global-set-key (kbd "M-<right>") 'next-buffer)
 ;; ignore useless buffers while switching，切换时忽略掉 * 打头的 buffer
 (set-frame-parameter (selected-frame) 'buffer-predicate (lambda (buf) (not (string-match-p "^*" (buffer-name buf)))))
 
-;; list-buffers
-(global-set-key (kbd "C-x C-b") 'list-buffers)
 
-(global-set-key (kbd "C-M-\\") 'indent-region-or-buffer)
 
 
 
@@ -87,60 +83,6 @@
 
 
 
-
-
-(provide 'init-keybindings)
-
-
-(global-set-key (kbd "<f2>") 'my-open-init-file)
-
-(global-set-key (kbd "s-/") 'hippie-expand)
-
-(global-set-key "\C-x\ \C-r" 'recentf-open-files)
-
-
-;; 注意和 C-h f/v/k 的区别，它们是describe function/variable/key，
-;; 而这里是 find function/variable/key
-(global-set-key (kbd "C-h C-f") 'find-function)
-(global-set-key (kbd "C-h C-v") 'find-variable)
-(global-set-key (kbd "C-h C-k") 'find-function-on-key)
-
-
-(with-eval-after-load 'dired  ;;dired-mode-map必须要在Dired-Mode进入后才有效，所以这里加这句
-  (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file))
-
-
-;; -----------直接用Ctrl+TAB切换Buffer------------
-;; -----默认切换Buffer是"C-x b"，较繁琐，改为C-TAB提高效率--------
-(global-set-key (kbd "C-<tab>") #'(lambda ()
-				    (interactive)
-				    (switch-to-buffer (other-buffer (current-buffer) 1))))
-
-
-
-(global-set-key (kbd "C-M-\\") 'indent-region-or-buffer)
-
-
-
-;; occur-mode下也可以使用hjkl来移动
-(add-hook 'occur-mode-hook
-	  (lambda ()
-	    (evil-add-hjkl-bindings occur-mode-map 'emacs
-	      (kbd "/")       'evil-search-forward
-	      (kbd "n")       'evil-search-next
-	      (kbd "N")       'evil-search-previous
-	      (kbd "C-d")     'evil-scroll-down
-	      (kbd "C-u")     'evil-scroll-up
-	      )))
-
-
-
-
-
-(global-set-key (kbd "C-c a") 'org-agenda)
-
-
-
 ;; evil模式下可以使用C-u的设置
 (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
 (define-key evil-visual-state-map (kbd "C-u") 'evil-scroll-up)
@@ -151,14 +93,13 @@
 (evil-leader/set-leader "SPC") ;; 将空格键设置为leader-key
 (evil-leader/set-key
   
-  ;; "<left>" 'previous-bffer 
   "ff" 'find-file
   "fr" 'recentf-open-files
-  "bb" 'switch-to-buffer
+  "bl" 'list-buffers
+  "bs" 'switch-to-buffer
   "bk" 'kill-buffer
-  "pf" 'counsel-git ;; git项目下文件名搜索
-  "pt" 'counsel-git-grep ;; git项目下全文搜索
-  "ps" 'helm-do-ag-project-root ;; project-search
+  "pf" 'counsel-git ;; git项目下find-file
+  "pg" 'counsel-git-grep ;; git项目下grep
   
   ;;------- vim下窗口操作配置-----------
   ;; 切换窗口
@@ -175,17 +116,12 @@
   
   "w/" 'split-window-right ;; 右分屏
   "w-" 'split-window-below ;; 下分屏
-  "wm" 'delete-other-windows ;; 相当于emacs下"C-x 1"
+  "w1" 'delete-other-windows ;; 相当于emacs下"C-x 1"
   
   "xf" 'counsel-find-file
-  "xr" 'recentf-open-files
-
-  "so" 'sr-speedbar-open
-  "sc" 'sr-speedbar-close
 
   ":" 'counsel-M-x
   )
 
-(global-set-key (kbd "C-=") 'er/expand-region)
 
 (provide 'init-keybindings)
